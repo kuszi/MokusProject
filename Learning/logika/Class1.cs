19e1dd7466c4e8af88aee536f0fdf55db3f5082e
prins <prins@DESKTOP-D87UGNR>
2019-01-29 22:01:38 +0100

    logika1

---
+++ Learning/logika/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logika
+{
+    class Class1
+    {
+        public static int c;
+        public void szamol()
+        {
+            //szamoljunk el egytol tizig,
+            // irjuk ki az 5nél nagyobbakat
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i > 5)
+                {
+
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+
+        public void szamol2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                c++;
+            }
+        }
+    }
+}
---
+++ Learning/logika/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 b = new Class1();
+            Class1 a = new Class1();
+            a.szamol();
+            a.szamol2();
+            a.szamol2();
+            Console.WriteLine(Class1.c);
+            b.szamol2();
+            Console.WriteLine(Class1.c);
+
+            Console.WriteLine("nyomj meg egy gombot");
+            Console.ReadKey();
+
+        }
+    }
+}
