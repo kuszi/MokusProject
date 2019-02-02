2b9bf2ed36eb32033079aeecd4b63526e6de3e2f
prins <prins@DESKTOP-D87UGNR>
2019-02-02 09:48:57 +0100

    3 házi, kezdetek

---
+++ Learning/HArmadikHaziSzazig/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HArmadikHaziSzazig
+{
+    class Program
+    {
+//        Generálj 100 darab véletlenszámot 0 és 100 között
+//https://stackoverflow.com/questions/2706500/how-do-i-generate-a-random-int-number-in-c
+
+//Írd ki a képernyőre, hogy hány darab 50-nél kissebb, illetve 50-nél nagyobb volt.
+//Az 50 az az 50-nél nagyobb kategóriába megy
+
+//írd ki továbbá a legkisebb és a legnagyobb generált számot
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
