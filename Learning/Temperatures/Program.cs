23b19956b26b4baccdcb68deff2be4e1f151183d
goldschmidt csaba <kusziworks@gmail.com>
2019-03-12 19:38:07 +0100

    kiss help :)

--- Learning/Temperatures/Program.cs
+++ Learning/Temperatures/Program.cs
@@ -27,23 +27,34 @@ namespace Temperatures
    
             //int n = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
             int[] inputs = {1, 6, 2, 3, -5, 4,-6,-2,-1,-4 ,0};//Console.ReadLine().Split(' ');
-            int aMAxValue = 6;
+
+            // *** Na ennek kéne int.MaxValue -nak lennie
+            int aMAxValue =  6;
             for (int i = 0; i < inputs.Length; i++)
             {
+                
                 if (aMAxValue > Math.Abs(inputs[i]))
                 {
                     aMAxValue = Math.Abs(inputs[i]);
                 }
-                else if (aMAxValue == Math.Abs(inputs[i]) && inputs[i] > aMAxValue)
-                    {
+                // *** Ez a feltétel sose tud teljesülni.
+                // *** gondolj bele, a aMAxValue a legnagyobbat tartalmazza, azaz Mindig Pozitiv. Ha már egyszer egyenlő, akkor utána már az inputs[i] nem tud nagyobb lenni
+                // *** De azt nagyon jól látod, hogy egy ehhez hasonló if ide KELLENI FOG :) 
+                else if (aMAxValue == Math.Abs(inputs[i]) && inputs[i] > aMAxValue)  
+                {
                     aMAxValue = inputs[i];
                 }
+                // *** felesleges. a legelső if megoldja ezt a problémát
                 else if (i == 0)
                 {
                     aMAxValue = inputs[i];
                 }
                 //int t = int.Parse(inputs[i]); // a temperature expressed as an integer ranging from -273 to 5526
             }
+
+            // *** KISS segítség :)
+            // *** az már nagyon jó, hogy az aMAxValue-ban tárolod a távolságot
+            // *** de mivel ez eredetileg lehetett egy negatív szám is, lehet hogy azt is tárolni kéne :)
 
             // Write an action using Console.WriteLine()
             // To debug: Console.Error.WriteLine("Debug messages...");
