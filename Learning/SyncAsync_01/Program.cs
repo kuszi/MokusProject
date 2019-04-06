124c808fbc55fa8c13778e330a8a58e9d102922d
goldschmidt csaba <kusziworks@gmail.com>
2019-04-06 08:49:02 +0200

    Async01

---
+++ Learning/SyncAsync_01/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncAsync_01
+{
+    class Program
+    {
+        // Ebben a kis programban a fő program szálon "*" karaketereket írunk ki
+        // Egy párhuzamosan futo Task-ban pedig "-" karaktereket
+
+        // A két kód egymás mellett párhuzamosan fut. 
+        // A windows gondoskodik arról, hogy mindkét programszál (task) kapjon egy kis időszeletet a futáshoz
+
+        // ha többször egymás után futtatod, akkor teljesen más kimenetet kaphasz
+        // Ez a Windows szálütemezője miatt van. Hol az egyik szál kap egy kis időt, hol a maásik. A kis időt viszont mindig más
+
+        // HÁZI FELADAT
+        // 1. megérteni, ha kérdés van, akkor kérdezni
+        // 2. Vegyél fel mégy egy taskot, ami "T" karaktereket ír ki
+        // 3. vegyél fel pluszmég egy taskot, ami "+" jeleket ír ki
+        // 4. A főprogram a "*" karakterek kiírása után írjon ki 100 db "#" jelet
+
+        // Ha a 4. ponttal is kész vagy, láthatod, a "#" karakterek mindig az összes "*" kirakása után van.
+        // Ez logikus, hiszen ez a szinkron futás (amikor az egyik művelet a másik után hajtódik végre)
+        // Ezzel szemben a "-", "T" és "+" jelek bárhol megjelenhetnek, mert külön szálon (külön taskban) futnak. Ez az asyncron futás
+        static void Main(string[] args)
+        {
+            
+            Task task = Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write("-");
+                    Thread.Sleep(1);
+                }
+            });
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("*");
+                Thread.Sleep(1);
+            }
+
+
+            Console.WriteLine("Done. Press Enter");
+            Console.ReadLine();
+        }
+    }
+}
