cbd425313679f46d47e4cf120393cf71ed32cfe0
prins <prins@DESKTOP-D87UGNR>
2019-10-20 15:52:23 +0200

    ChessGames #1

---
+++ Learning/ChessGames/Classes/kiraly.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGames.Classes
+{
+    class Kiraly : IBabu
+    {
+        public string BabuNev
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string BabuPozicio
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool BabuSzine
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void BabuMozgas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BabuUtes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Learning/ChessGames/Interfaces/IBabu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGames
+{
+    interface IBabu
+    {
+        string BabuNev { get; set; }
+        bool BabuSzine { get; set; }
+        void BabuMozgas();
+        string BabuPozicio { get; set; }
+        void BabuUtes();
+    }
+}
