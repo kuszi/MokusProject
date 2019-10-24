71d66e327123c5410b9ca72deee3ece4064a945d
prins <prins@DESKTOP-D87UGNR>
2019-10-24 19:20:01 +0200

    converterek

---
+++ Learning/ChessGames/Converter/PositionLeftConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ChessGames.Converter
+{
+    public class PositionLeftConverter : IValueConverter            //betű
+
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valami = value as string;
+            int retVal = 0;
+            if (valami != null && valami.Length == 2)
+            {
+                switch (valami[0])
+                {
+                    case 'a':
+                        retVal = 0;
+                        break;   
+                    case 'b':
+                        retVal = 100;
+                        break;      
+                    case 'c':
+                        retVal = 200;
+                        break;      
+                    case 'd':
+                        retVal = 300;
+                        break;      
+                    case 'e':
+                        retVal = 400;
+                        break;
+                    case 'f':
+                        retVal = 500;
+                        break;
+                    case 'g':
+                        retVal = 600;
+                        break;
+                    case 'h':
+                        retVal = 800;
+                        break;      
+                }
+                return retVal;     
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Learning/ChessGames/Converter/PositionTopConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ChessGames.Converter
+{
+    public class PositionTopConverter : IValueConverter        //szám   
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valami = value as string;
+            int retVal = 0;
+            if (valami != null && valami.Length == 2)
+            {
+                switch (valami[1])
+                {
+                    case '1':
+                        retVal = 0;
+                        break;
+                    case '2':
+                        retVal = 100;
+                        break;
+                    case '3':
+                        retVal = 200;
+                        break;
+                    case '4':
+                        retVal = 300;
+                        break;
+                    case '5':
+                        retVal = 400;
+                        break;
+                    case '6':
+                        retVal = 500;
+                        break;
+                    case '7':
+                        retVal = 600;
+                        break;
+                    case '8':
+                        retVal = 700;
+                        break;
+                }
+                return retVal;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
