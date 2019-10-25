76615ee18e475bb34e67752c32ec99482e1cea25
prins <prins@DESKTOP-D87UGNR>
2019-10-25 17:44:38 +0200

    babuk színesek

---
+++ Learning/ChessGames/Converter/BabuColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ChessGames.Converter
+{
+    public class BabuColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valami = (bool)value;
+            string returnValue = "0";
+            if (valami == true)
+            {
+                returnValue = "White";
+                return returnValue;
+            }
+            else
+            {
+                returnValue = "Black";
+                return returnValue;
+            }
+
+         //   return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
